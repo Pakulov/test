dce9a68499d1b310c37b821bf2d14cd939a46c43
Eugen Pakulov <ev-pakulov@mail.ru>
2015-07-02 21:51:07 +0300

    commit 3

--- index.php
+++ index.php
@@ -29,14 +29,13 @@
 		<a href="#" id="clicky">link</a>
 	</div>
 	<div id="footer">
-		<h4>Дбавил футер</h4>
-		<div>
-			<span>FOOteR</span>
-		</div>
-		<h4>Добавил футер</h4>
-		<div>
-			<span>FOOteR</span>
-		</div>
+		<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
+
 	</div>
 
 <!--jQuery js-->
